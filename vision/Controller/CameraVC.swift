3931320bc2f1bc51345cefe1796f34d515472a3e
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2018-01-10 17:31:38 +0100

    added flash

--- vision/Controller/CameraVC.swift
+++ vision/Controller/CameraVC.swift
@@ -11,6 +11,11 @@ import AVFoundation
 import CoreML
 import Vision
 
+enum FlashState {
+    case off
+    case on
+}
+
 class CameraVC: UIViewController {
     
     var captureSession: AVCaptureSession!
@@ -18,6 +23,8 @@ class CameraVC: UIViewController {
     var previewLayer: AVCaptureVideoPreviewLayer!
     
     var photoData: Data?
+    
+    var flashControlState: FlashState = .off
 
     //Outlets
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
@@ -29,7 +36,7 @@ class CameraVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -79,6 +86,12 @@ class CameraVC: UIViewController {
         
         settings.previewPhotoFormat = previewFormat
         
+        if flashControlState == .off {
+            settings.flashMode = .off
+        } else {
+            settings.flashMode = .on
+        }
+        
         cameraOutput.capturePhoto(with: settings, delegate: self)
     }
     
@@ -97,6 +110,17 @@ class CameraVC: UIViewController {
                 break
             }
             
+        }
+    }
+    
+    @IBAction func flashBtnWasPressed(_ sender: Any) {
+        switch flashControlState {
+        case .off:
+            flashBtn.setTitle("FLASH ON", for: .normal)
+            flashControlState = .on
+        case .on:
+            flashBtn.setTitle("FLASH OFF", for: .normal)
+            flashControlState = .off
         }
     }
     
